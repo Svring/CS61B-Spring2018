a6e61d1ecfccc907ad207134c13dbc420ed2b3bd
Svring <lxf83553899@163.com>
2022-08-01 18:10:45 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,12 +81,15 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A.rest == null) {
-            A.rest = B;
-            return A;
-        } else {
-            return dcatenate(A.rest, B);
+        if(A == null){
+            return B;
         }
+        IntList ptr = A;
+        while(ptr.rest != null){
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
