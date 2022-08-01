ac2c3d4db54f95721a868c2e7f5f025b699a9acc
Svring <lxf83553899@163.com>
2022-08-01 10:44:19 +0800

    DebugPractice

--- labs/lab2/DebugPractice/DebugExercise3.java
+++ labs/lab2/DebugPractice/DebugExercise3.java
@@ -5,9 +5,9 @@ public class DebugExercise3 {
     public static int countTurnips(In in) {
         int totalTurnips = 0;
         while (!in.isEmpty()) {
-            String vendor = in.readString();
+            //String vendor = in.readString();
             String foodType = in.readString();
-            double cost = in.readDouble();
+            //double cost = in.readDouble();
             int numAvailable = in.readInt();
             if (foodType.equals("turnip")) {
                 int newTotal = totalTurnips + numAvailable;
