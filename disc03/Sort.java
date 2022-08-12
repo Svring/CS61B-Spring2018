391fe3fe90e4bae666f06c45d3376f9b61e6fb17
Svring <lxf83553899@163.com>
2022-08-12 09:04:29 +0800

    disc03

--- disc03/Sort.java
+++ disc03/Sort.java
@@ -1,16 +1,23 @@
 public class Sort {
     /** Sorts strings destructively. */
     public static void sort(String[] x) { 
-           // find the smallest item
-           // move it to the front
-           // selection sort the rest (using recursion?)
-           
+           sort(x, 0);
+    }
+
+    private static void sort(String[] x, int start) {
+        if (start == x.length) {
+            return;
+        } else {
+            int smallest = findSmallest(x, start);
+            swap(x, start, smallest);
+            sort(x, start + 1);
+        }
     }
 
     /** Returns the smallest string in x. */
-    public static int findSmallest(String[] x) {
-        int smallest = 0;
-        for (int i = 0; i < x.length; i++) {
+    public static int findSmallest(String[] x, int start) {
+        int smallest = start;
+        for (int i = start; i < x.length; i++) {
             if (x[smallest].compareTo(x[i]) > 0) {
                 smallest = i;
             }
--- disc03/TestSort.java
+++ disc03/TestSort.java
@@ -1,19 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestSort {
-    public static void testFindSmallest() {
+    @Test
+    public void testFindSmallest() {
         String[] input = {"i", "have", "an", "egg"};
         int expected = 2;
-    
-        int actual = Sort.findSmallest(input);
-        org.junit.Assert.assertEquals(expected, actual);        
-    
-        String[] input2 = {"there", "are", "many", "pigs"};
-        int expected2 = 1;
-    
-        int actual2 = Sort.findSmallest(input2);
-        org.junit.Assert.assertEquals(expected2, actual2);
-    }
 
-    public static void main(String[] args) {
-        testFindSmallest(); // note: we changed this from testSort!
+        int actual = Sort.findSmallest(input, 0);
+        assertEquals(expected, actual);        
+
+        String[] input2 = {"there", "are", "many", "pigs"};
+        int expected2 = 2;
+
+        int actual2 = Sort.findSmallest(input2, 2);
+        assertEquals(expected2, actual2);
     }
 }
