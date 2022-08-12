d935196cbe623629df7e0e44b2ddd909f537e67e
Svring <lxf83553899@163.com>
2022-08-12 10:29:25 +0800

    proj0 update

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,6 +6,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
+    public static double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV,double yV, double m, String img) {
         xxPos = xP;
@@ -25,4 +26,55 @@ public class Planet {
         imgFileName = p.imgFileName;
     }
     
+    public double calcDistance(Planet p) {
+        double distance;
+        double Xdis = p.xxPos - xxPos;
+        double Ydis = p.yyPos - yyPos;
+        distance = Math.sqrt(Xdis * Xdis + Ydis * Ydis);
+        return distance;
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double force;
+        force = G * ((mass * p.mass) / Math.pow(calcDistance(p), 2));
+        return force;
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double force;
+        double Xdis = p.xxPos - xxPos;
+        force = calcForceExertedBy(p) * (Xdis / calcDistance(p));
+        return force;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double force;
+        double Ydis = p.yyPos - yyPos;
+        force = calcForceExertedBy(p) * (Ydis / calcDistance(p));
+        return force;
+    }
+
+    public double calcNetForceExertedByX(Planet[] p) {
+        double force = 0;
+        for (int i = 0; i < p.length; i++) {
+            if (this.equals(p[i])) {
+                continue;
+            } else {
+                force += calcForceExertedByX(p[i]);
+            }
+        }
+        return force;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p) {
+        double force = 0;
+        for (int i = 0; i < p.length; i++) {
+            if (this.equals(p[i])) {
+                continue;
+            } else {
+                force += calcForceExertedByY(p[i]);
+            }
+        }
+        return force;
+    }
 }
