0106dfe0e24005e2d889923763d8b5830d9c6c16
Svring <lxf83553899@163.com>
2022-08-13 16:59:03 +0800

    fix API

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,7 +6,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static double G = 6.67e-11;
+    private static double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV,double yV, double m, String img) {
         xxPos = xP;
@@ -78,7 +78,7 @@ public class Planet {
         return force;
     }
 
-    public void update(double dt, Double forceX, Double forceY) {
+    public void update(double dt, double forceX, double forceY) {
         double accelerateX = forceX / mass;
         double accelerateY = forceY / mass;
         xxVel += accelerateX * dt;
