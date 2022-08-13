0bc5652b0d5493990e3c4ed7fa6074565cc37959
Svring <lxf83553899@163.com>
2022-08-13 16:52:54 +0800

    proj0 commit

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -33,8 +33,8 @@ public class NBody {
         return a;
     }
 
-    @SuppressWarnings("unused")
     public static void main(String[] args) {
+        /* canvas */
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
@@ -43,5 +43,37 @@ public class NBody {
         StdDraw.setScale(-radius, radius);
         StdDraw.picture(0, 0, "images/starfield.jpg");
         for (Planet p: planets) { p.draw();}
+
+        /* animation */
+        StdDraw.enableDoubleBuffering();
+        double time = 0;
+        while (time != T) {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for (int i = 0; i < planets.length; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+            for (Planet p: planets) { p.draw();}
+            StdDraw.show();
+            StdDraw.pause(10);
+            time += dt;
+        }
+
+        /* final print */
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+        }
     }
 }
+
+/* I have to say, this is the most exciting game I've ever made */
+/* Though the style of my code isn't that beautiful, I'll fix this next time */
