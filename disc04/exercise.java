61f1d8346a9c185207445bcffb7371da59d67948
Svring <lxf83553899@163.com>
2022-08-26 10:07:38 +0800

    exercise

---
+++ disc04/exercise.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class exercise {
+    public static List<String> getwords(String filename) {
+        In in = new In(filename);
+        List<String> L = new ArrayList<String>();
+        String word = in.readString();
+        while (word != null) {
+            L.add(word);
+            word = in.readString();
+        }
+        return L;
+    }
+
+    public static int countUniqueWords(List<String> L) {
+        Set<String> result = new HashSet<String>(L);
+        return result.size();
+    }
+
+    public static Map<String, Integer> collectWordCount(List<String> targets, List<String> words) {
+        Map<String, Integer> ans = new HashMap<>();
+        for (String s: targets) {
+            int time = 0;
+            for (String r: words) {
+                if (s.equals(r)) {
+                    time++;
+                }
+            }
+            ans.put(s, time);
+        }
+        return ans;
+    }
+}
