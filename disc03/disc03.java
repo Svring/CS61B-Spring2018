9e39b11175287ad99c66e8092f92ad175d8a15b1
Svring <lxf83553899@163.com>
2022-08-27 15:59:48 +0800

    examprep4

--- disc03/disc03.java
+++ disc03/disc03.java
@@ -1,95 +1,98 @@
-class SLList {
-    public class IntNode {
-        public int item;
-        public IntNode next;
-        public IntNode(int item, IntNode next) {
-            this.item = item;
-            this.next = next;
+public class disc03 {
+    @SuppressWarnings("unused")
+    private class SLList {
+        public class IntNode {
+            public int item;
+            public IntNode next;
+            public IntNode(int item, IntNode next) {
+                this.item = item;
+                this.next = next;
+            }
         }
-    }
 
-    private IntNode first;
+        private IntNode first;
 
-    public void addFirst(int x) {
-        first = new IntNode(x, first);
-    }
+        public void addFirst(int x) {
+            first = new IntNode(x, first);
+        }
 
-    public void insert(int item, int pos) {
-        IntNode p = this.first;
-        while (pos > 1) {
-            if (p.next == null) {
-                p.next = new IntNode(item, null);
-                return;
+        public void insert(int item, int pos) {
+            IntNode p = this.first;
+            while (pos > 1) {
+                if (p.next == null) {
+                    p.next = new IntNode(item, null);
+                    return;
+                } else {
+                    p = p.next;
+                    pos--;
+                }
+            }
+            p.next = new IntNode(item, p.next);
+        }
+
+        public void reverse() {
+            IntNode front = null;
+            IntNode current = first;
+            while (current != null) {
+                IntNode remind = current.next;
+                current.next = front;
+                front = current;
+                current = remind;
+            }
+            first = front;
+        }
+
+        public void reverserecur() {
+            first = reverserecursion(first);
+        }
+
+        public IntNode reverserecursion(IntNode front) {
+            if (front == null || front.next == null) {
+                return front;
             } else {
-                p = p.next;
-                pos--;
+                IntNode reversed = reverserecursion(front.next);
+                front.next.next = front;
+                front.next = null;
+                return reversed;
             }
         }
-        p.next = new IntNode(item, p.next);
-    }
 
-    public void reverse() {
-        IntNode front = null;
-        IntNode current = first;
-        while (current != null) {
-            IntNode remind = current.next;
-            current.next = front;
-            front = current;
-            current = remind;
-        }
-        first = front;
-    }
-
-    public void reverserecur() {
-        first = reverserecursion(first);
-    }
-
-    public IntNode reverserecursion(IntNode front) {
-        if (front == null || front.next == null) {
-            return front;
-        } else {
-            IntNode reversed = reverserecursion(front.next);
-            front.next.next = front;
-            front.next = null;
-            return reversed;
-        }
-    }
-
-    public static int[] insert(int[] arr, int item, int position) {
-        int[] a = new int[arr.length + 1];
-        if (position >= arr.length) {
-            System.arraycopy(arr, 0, a, 0, arr.length);
-            a[arr.length] = item;
-        } else {
-            System.arraycopy(arr, 0, a, 0, position);
-            a[position] = item;
-            System.arraycopy(arr, position, a, position + 1, arr.length - position);
-        }
-        return a;
-    }
-
-    public static void reverse(int[] arr) {
-        for (int i = 0; i <= (arr.length / 2); i++) {
-            int tmp = arr[i];
-            arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = tmp;
-        }
-    }
-
-    public static int[] replicate(int[] arr) {
-        int sum = 0, index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+        public static int[] insert(int[] arr, int item, int position) {
+            int[] a = new int[arr.length + 1];
+            if (position >= arr.length) {
+                System.arraycopy(arr, 0, a, 0, arr.length);
+                a[arr.length] = item;
+            } else {
+                System.arraycopy(arr, 0, a, 0, position);
+                a[position] = item;
+                System.arraycopy(arr, position, a, position + 1, arr.length - position);
+            }
+            return a;
         }
 
-        int[] a = new int[sum];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[index]; j++) {
-                a[index] = arr[i];
-                index++;
+        public static void reverse(int[] arr) {
+            for (int i = 0; i <= (arr.length / 2); i++) {
+                int tmp = arr[i];
+                arr[i] = arr[arr.length - i - 1];
+                arr[arr.length - i - 1] = tmp;
             }
         }
-        return a;
-    }
 
+        public static int[] replicate(int[] arr) {
+            int sum = 0, index = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sum += arr[i];
+            }
+
+            int[] a = new int[sum];
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[index]; j++) {
+                    a[index] = arr[i];
+                    index++;
+                }
+            }
+            return a;
+        }
+
+    }
 }
--- disc04/examprep4.java
+++ disc04/examprep4.java
@@ -1,3 +1,58 @@
+import java.util.NoSuchElementException;
+
 public class examprep4 {
-    
+
+    public class SLList {
+        int item;
+        SLList next;
+        public SLList() {}
+        public void insertFront(int x) {}
+        public int indexOf(int x) {return 0;}
+    }
+
+    public class SLListVista extends SLList {
+        @Override
+        public int indexOf(int x) {
+            int index = super.indexOf(x);
+            if (index == -1) {
+                throw new NoSuchElementException();
+            }
+            return index;
+        }
+    }
+
+    public class DMSList {
+        private IntNode sentinel;
+
+        public DMSList() {
+            sentinel = new IntNode(-1000, new node());
+        }
+
+        public int max() {
+            return sentinel.next.max();
+        }
+
+        public class IntNode {
+            public int item;
+            public IntNode next;
+            public IntNode(int i, IntNode h) {
+                item = i;
+                next = h;
+            }
+            public int max() {
+                return Math.max(item, next.max());
+            }
+        }
+
+        public class node extends IntNode {
+            public node() {
+                super(0, null);
+            }
+
+            @Override
+            public int max() {
+                return 0;
+            }
+        }
+    }
 }
