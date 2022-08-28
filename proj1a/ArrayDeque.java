b3a01a7c852f97af0fbd5b8f203ff1156ea1620c
Svring <lxf83553899@163.com>
2022-08-28 15:40:42 +0800

    proj1a complete

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,141 @@
+public class ArrayDeque<T> implements Deque<T> {
+
+    private T[] items;
+    private int size;
+    private int length;
+    private int nextFirst;
+    private int nextLast;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int minusOne(int index) {
+        if (index == 0) {
+            return length - 1;
+        }
+        return index - 1;
+    }
+
+    private int plusOne(int index, int module) {
+        index %= module;
+        if (index == module - 1) {
+            return 0;
+        }
+        return index + 1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayDeque() {
+        items = (T []) new Object[8];
+        size = 0;
+        length = 8;
+        nextFirst = 4;
+        nextLast = 4;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (size == length - 1) {
+            resize();
+        }
+        nextFirst = minusOne(nextFirst);
+        items[nextFirst] = item;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (size == length - 1) {
+            resize();
+        }
+        items[nextLast] = item;
+        nextLast = plusOne(nextLast, length);
+        size++;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (length >= 16 && length / size >= 4) {
+            resize();
+        }
+        if (size == 0) {
+            return null;
+        }
+        T ans = items[nextFirst];
+        nextFirst = plusOne(nextFirst, length);
+        size--;
+        return ans;
+    }
+
+    @Override
+    public T removeLast() {
+        if (length >= 16 && length / size >= 4) {
+            resize();
+        }
+        if (size == 0) {
+            return null;
+        }
+        nextLast = minusOne(nextLast);
+        size--;
+        return items[nextLast];
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        int p = nextFirst;
+        for (int i = 0; i < index; i++) {
+            p = plusOne(p, length);
+        }
+        return items[p];
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        if (size == length - 1) {
+            T[] newitems = (T []) new Object[length * 2];
+            int ptr = length;
+            int front = nextFirst;
+            while (front != nextLast) {
+                newitems[ptr] = items[front];
+                front = plusOne(front, length);
+                ptr = plusOne(ptr, length * 2);
+            }
+            nextFirst = length;
+            nextLast = ptr;
+            items = newitems;
+            length *= 2;
+        } else if (length >= 16 && length / size >= 4) {
+            T[] newitems = (T []) new Object[length / 2];
+            int ptr = length / 4;
+            int front = nextFirst;
+            while (front != nextLast) {
+                newitems[ptr] = items[front];
+                front = plusOne(front, length);
+                ptr = plusOne(ptr, length / 2);
+            }
+            nextFirst = length / 4;
+            nextLast = ptr;
+            items = newitems;
+            length /= 2;
+        }
+    }
+
+    @Override
+    public void printDeque() {
+        int p = nextFirst;
+        while (p != nextLast) {
+            System.out.print(items[p] + " ");
+            p = plusOne(p, length);
+        }
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,121 @@
+public class LinkedListDeque<T> implements Deque<T> {
+
+    /* abstraction level one */
+    private class Node {
+
+        /* instance variebles */
+        private T item;
+        private Node next;
+        private Node prev;
+
+        /* initialize */
+        public Node(T i, Node n, Node p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+        public Node() {
+            item = null;
+            next = null;
+            prev = null;
+        }
+    }
+
+    /* abstraction level two */
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T var) {
+        Node newList = new Node(var, sentinel.next, sentinel);
+        sentinel.next.prev = newList;
+        sentinel.next = newList;
+        size++;
+    }
+
+    @Override
+    public void addLast(T var) {
+        Node newList = new Node(var, sentinel, sentinel.prev);
+        sentinel.prev.next = newList;
+        sentinel.prev = newList;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        Node p = sentinel.next;
+        while (p != sentinel) {
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        Node p = sentinel.next;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return p.item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        Node p = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return p.item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        Node p = sentinel.next;
+        while (index > 0) {
+            p = p.next;
+            index--;
+        }
+        return p.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index >= size) {
+            return null;
+        }
+        return helper(index, sentinel.next);
+    }
+
+    private T helper(int index, Node L) {
+        if (index == 0) {
+            return L.item;
+        } else {
+            return helper(index - 1, L.next);
+        }
+    }
+}
