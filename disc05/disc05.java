2331653aa0bdbfcd333c33065ee39389655372f2
Svring <lxf83553899@163.com>
2022-08-30 16:04:49 +0800

    disc05 complete

---
+++ disc05/disc05.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class disc05 {
+
+    public class Queue<T> {
+
+        Stack<T> in = new Stack<>();
+        Stack<T> out = new Stack<>();
+
+        public void push(T item) {
+            check();
+            in.push(item);
+        }
+
+        public T pop() {
+            check();
+            return out.pop();
+        }
+
+        private void check() {
+            if (in.isEmpty()) {
+                while (! out.isEmpty()) {
+                    in.push(out.pop());
+                }
+            } else if (out.isEmpty()) {
+                while (! in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+        }
+    }
+}
