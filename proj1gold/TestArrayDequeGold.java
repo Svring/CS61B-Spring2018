0b07d1de22dbc298512e93c6b620108fcc7b69c5
Svring <lxf83553899@163.com>
2022-08-31 21:02:51 +0800

    proj1gold first-session

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public static void test() {
+        StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}
