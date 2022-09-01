92979eec0e47abee2fe77685c67dec6d23352456
Svring <lxf83553899@163.com>
2022-09-01 22:04:20 +0800

    proj1gold update

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,7 +5,7 @@ public class TestArrayDequeGold {
     @Test
     public void test() {
         StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
-        ArrayDeque<Integer> exp = new ArrayDeque<>();
+        ArrayDequeSolution<Integer> exp = new ArrayDequeSolution<>();
         for (int i = 0; i < 10; i++) {
             double factor = StdRandom.uniform();
             if (factor < 0.25) {
@@ -18,19 +18,29 @@ public class TestArrayDequeGold {
                 if (stu.isEmpty() || exp.isEmpty()) {
                     continue;
                 }
-                stu.removeFirst();
-                exp.removeFirst();
+                Integer actual = stu.removeFirst();
+                Integer expect = exp.removeFirst();
+                assertEquals(expect, actual);
             } else {
                 if (stu.isEmpty() || exp.isEmpty()) {
                     continue;
                 }
-                stu.removeLast();
-                exp.removeLast();
+                Integer actual = stu.removeLast();
+                Integer expect = exp.removeLast();
+                assertEquals(expect, actual);
             }
+            /*
             assertEquals(exp.size(), stu.size());
             for (int j = 0; j < stu.size(); j++) {
-                assertEquals(exp.get(j), stu.get(j));
+                Integer expect = exp.get(j);
+                Integer actual = stu.get(j);
+                assertEquals(expect, actual);
             }
+            */
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
