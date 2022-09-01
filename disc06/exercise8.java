4aaf0982e6c6599dae1655b8e76726488c055c29
Svring <lxf83553899@163.com>
2022-09-01 17:19:06 +0800

    exercise8 setup

---
+++ disc06/exercise8.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class exercise8 {
+    private class Stack<Item> {
+        private List<Item> l = new LinkedList<>();
+        public void push(Item x) {
+            l.add(x);
+        }
+    }
+}
