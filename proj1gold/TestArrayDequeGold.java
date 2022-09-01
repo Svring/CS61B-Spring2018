a1de92580ad93c5c6d9df32e4d3f679dde06fccc
Svring <lxf83553899@163.com>
2022-09-01 22:44:26 +0800

    proj1gold complete

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,46 +1,54 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+/* The only thing could put you down is THE AMOUNT OF DATA */
 public class TestArrayDequeGold {
     @Test
     public void test() {
         StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> exp = new ArrayDequeSolution<>();
-        for (int i = 0; i < 10; i++) {
+        ArrayDequeSolution<String> messageBox = new ArrayDequeSolution<>();
+        String message;
+        for (int i = 0; i < 1000; i++) {
             double factor = StdRandom.uniform();
             if (factor < 0.25) {
+                message = "addFirst" + "(" + i + ")";
                 stu.addFirst(i);
                 exp.addFirst(i);
             } else if (factor >= 0.25 && factor < 0.5) {
+                message = "addLast" + "(" + i + ")";
                 stu.addLast(i);
                 exp.addLast(i);
             } else if (factor >= 0.5 && factor <0.75) {
                 if (stu.isEmpty() || exp.isEmpty()) {
                     continue;
                 }
+                message = "removeFirst()";
                 Integer actual = stu.removeFirst();
                 Integer expect = exp.removeFirst();
-                assertEquals(expect, actual);
+                assertEquals(ErrorMessage(messageBox, message), expect, actual);
             } else {
                 if (stu.isEmpty() || exp.isEmpty()) {
                     continue;
                 }
+                message = "removeLast()";
                 Integer actual = stu.removeLast();
                 Integer expect = exp.removeLast();
-                assertEquals(expect, actual);
+                assertEquals(ErrorMessage(messageBox, message), expect, actual);
             }
-            /*
-            assertEquals(exp.size(), stu.size());
-            for (int j = 0; j < stu.size(); j++) {
-                Integer expect = exp.get(j);
-                Integer actual = stu.get(j);
-                assertEquals(expect, actual);
+            messageBox.addLast(message);
+            if (messageBox.size() > 2) {
+                messageBox.removeFirst();
             }
-            */
         }
     }
 
-    public static void main(String[] args) {
-
+    private String ErrorMessage(ArrayDequeSolution<String> mb, String m) {
+        String message = "";
+        for (int i = 0; i < mb.size(); i++) {
+            message += mb.getFirst() + "\n";
+        }
+        message += m;
+        return message;
     }
 }
