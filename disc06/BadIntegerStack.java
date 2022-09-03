bca437871eb605677ae4195ac0972e696c31a867
Svring <lxf83553899@163.com>
2022-09-03 12:01:59 +0800

    BadIntegerStack update

--- disc06/BadIntegerStack.java
+++ disc06/BadIntegerStack.java
@@ -30,11 +30,20 @@ public class BadIntegerStack {
     }
 
     public static void main(String[] args) {
+        /* 
         try {
             BadIntegerStack stack = new BadIntegerStack();
             stack.pop();
         } catch (NullPointerException e) {
             System.out.println("Success!");
         }
+        */
+
+        /* BadIntegerStack stack = new BadIntegerStack();
+        stack.push(1);
+        stack.top.prev = stack.top;
+        while (!stack.isEmpty()) {
+            stack.pop();
+        } */
     }
 }
